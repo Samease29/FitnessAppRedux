bfe6c9b317db1b27a919da2ec4908182187f1947
NoahTBray <bray9@marshall.edu>
2020-04-16 09:02:14 -0400

    Added Utilities folder along with Tdee.cs class

---
+++ FitnessAppRedux/Utilities/Tdee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessApp
+{
+    public class TDEE
+    {
+        public static double Bmr(string sex, double pound, double inch, double age)
+        {
+            double kg = pound * 0.453592;
+            double cm = inch * 2.54;
+            double bmr = 0;
+            //if-else statement can change but the calculations are there 76.203456 172.72
+            if (sex.Equals("M"))
+            {
+                bmr = (10 * kg) + (6.25 * cm) - (5 * age) + 5;
+            }
+            else if (sex.Equals("F"))
+            {
+                bmr = (10 * kg) + (6.25 * cm) - (5 * age) - 161;
+            }
+            return bmr;
+        }
+
+        public static double getTdee(string activity, double bmr)
+        {
+            //Sedentary(little to no exercise + work a desk job) = 1.2
+            //Lightly Active(light exercise 1 - 3 days / week) = 1.375
+            //Moderately Active(moderate exercise 3 - 5 days / week) = 1.55
+            //Very Active(heavy exercise 6 - 7 days / week) = 1.725
+            //Extremely Active(very heavy exercise, hard labor job, training 2x / day) = 1.9
+
+            //if-else statement can change but the calculations are there
+            //switch statement is technically more efficient but the low number of circumstantials doesn't make much of a difference
+            double tdee = 0;
+            if (activity.Equals("sedentary"))
+            {
+                tdee = 1.2 * bmr;
+            }
+            else if (activity.Equals("light"))
+            {
+                tdee = 1.375 * bmr;
+            }
+            else if (activity.Equals("moderate"))
+            {
+                tdee = 1.55 * bmr;
+            }
+            else if (activity.Equals("very"))
+            {
+                tdee = 1.725 * bmr;
+            }
+            else if (activity.Equals("extreme"))
+            {
+                tdee = 1.9 * bmr;
+            }
+            return tdee;
+        }
+    }
+}
